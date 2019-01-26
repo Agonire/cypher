7c0a0f8d4beb13260cbf8ee1b767b2aea88f8e35
Agonire <46533011+Agonire@users.noreply.github.com>
2019-01-26 20:12:17 +0200

    after creating .gitignore

--- cypher.py
+++ cypher.py
@@ -3,10 +3,6 @@ from abc import ABC, abstractmethod
 
 class Cypher():
 
-    # def __init__(self, value):
-    #     self.value = value
-    #     super().__init__()
-
     @abstractmethod
     def makeCypherText(plainText):
         pass
@@ -19,8 +15,25 @@ class Cypher():
 class Ceaser(Cypher):
 
     @staticmethod
-    def makeCypherText(plainText):
-        pass
+    def makeCypherText(plainText, key):
+        temp = 0
+        output = ""
+        for letter in plainText:
+            letterIndex = ord(letter)
+            if letterIndex >= 97 and letterIndex <= 122:
+                letterIndex -= 97
+                temp = 97
+            elif letterIndex >= 65 and letterIndex <= 90:
+                letterIndex -= 65
+                temp = 65
+            else:
+                output += letter
+                continue
+            letterIndex += key
+            letterIndex = (letterIndex%26)+temp
+            letter = chr(letterIndex)
+            output += letter
+        return output
 
     @staticmethod
     def makePlainText(cypherText):
--- test.py
+++ test.py
@@ -1,20 +1,26 @@
+import cypher
 
 def testCeaser():
     testPlain = ["alfpha", "omega", "zet purpose"]
     testCypher = ["bmgqib", "pnfhb", "afu qvsqptf"]
 
-    for i in range(3):
-        outpt = testPlain[i] == testCypher[i]
-        print(outpt)
-
-    for i in range(3):
-        outpt = testPlain[i] == testPlain[i]
-        print(outpt)
 
     # for i in range(3):
-    #     outpt = Ceaser.makeCypherText(testPlain[i], 1) == testCypher[i]
+    #     outpt = testPlain[i] == testCypher[i]
+    #     print(testPlain[i], testCypher[i])
     #     print(outpt)
-    #
+
+    for i in range(3):
+        outpt = cypher.Ceaser.makeCypherText(testPlain[i], 1) == testCypher[i]
+        # print(test(testPlain[i], 1), testCypher[i])
+        print(outpt)
+
     # for i in range(3):
     #     outpt = Ceaser.makePlainText(testCypher[i], 1) == testPlain[i]
     #     print(outpt)
+
+
+
+# test()
+
+testCeaser()
